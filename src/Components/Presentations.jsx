8fd5572b16e4d6fec42963c515468e20705f2e64
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-11 13:03:38 -0500

    adding images

--- src/Components/Presentations.jsx
+++ src/Components/Presentations.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Styles/Presentations.css'
 import fractal from '../Images/geometric.jpg'
+import bridge from '../Images/bridge.jpg'
+import sky from '../Images/sky.jpg'
 
 const Presentations = () => {
 
   //const [windowWidt, setWindowWidt] = useState((window.innerWidth))
-
   //window.onresize = function () {
   //  setWindowWidt((window.innerWidth))
   //}
 
-  //let arrayOfImages = 
 
   return (
     <div className='Video'>
-      <img src={fractal} alt="" />
+      <img src={sky} alt="" />
       <h1>WELCOME</h1>
     </div>
   )
