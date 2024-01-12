030c4ab18479161e3541a674e7ad2e441972227c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-12 12:22:16 -0500

    new components and other things

--- src/Components/Columns.jsx
+++ src/Components/Columns.jsx
@@ -4,14 +4,38 @@ import '../Styles/Columns.css'
 const Columns = () => {
     return (
         <div className="container">
+            <button>
             <img src="https://personal-data-base-of-photos.netlify.app/assets/8.3c3e05e1.jpg" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/7.263d9146.jpg" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/5.b3f65e56.jpg" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/3.c93764da.png" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/2.996cd67c.png" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/1.21598796.jpg" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/8.3c3e05e1.jpg" />
-            <img src="https://personal-data-base-of-photos.netlify.app/assets/7.263d9146.jpg" />
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/7.263d9146.jpg" />   
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/5.b3f65e56.jpg" />   
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/3.c93764da.png" />   
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/2.996cd67c.png" />   
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/1.21598796.jpg" />   
+            </button>
+            
+            <button>
+             <img src="https://personal-data-base-of-photos.netlify.app/assets/8.3c3e05e1.jpg" />   
+            </button>
+            
+            <button>
+              <img src="https://personal-data-base-of-photos.netlify.app/assets/7.263d9146.jpg" />  
+            </button>
+            
         </div >
     )
 }
