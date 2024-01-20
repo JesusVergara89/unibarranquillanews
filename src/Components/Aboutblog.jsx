b96fb54145ec96ba767f3e312ca804dd7464c2e7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-19 19:56:58 -0500

    changes 2

--- src/Components/Aboutblog.jsx
+++ src/Components/Aboutblog.jsx
@@ -9,6 +9,8 @@ const Aboutblog = () => {
             ¡Bienvenidos a la plataforma digital de noticias de la Universidad de Barranquilla!
             </p>
 
+            <a href="https://www.unibarranquilla.edu.co/" target="_blank">Página oficial de la universidad</a>
+
             <p>
             En Unibarranquilla Noticias, estamos emocionados de ser tu fuente de información y actualización constante sobre todo lo que acontece en nuestra vibrante comunidad universitaria. Aquí encontrarás una variedad de temas que reflejan la diversidad de intereses y actividades que enriquecen nuestra vida académica y estudiantil. ¡Explora, descubre y mantente conectado!
             </p>         
