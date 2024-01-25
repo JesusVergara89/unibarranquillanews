9293a9b30f3fcbeac7d15e074685980a6037993e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-25 12:35:47 +0100

    sport components

--- src/Components/Literature.jsx
+++ src/Components/Literature.jsx
@@ -6,21 +6,26 @@ const Literature = () => {
 
   const sports = useSport()
 
+  console.log(sports)
+
   return (
     <article className="engineering_section">
       <div className="visualization-div-header"></div>
       <h2 className="title-actualidad">
-        ACTUALIDAD UNIVERSITARIA
+        DEPORTES UNIVERSITARIOS
       </h2>
       <p className='description-actualidad'>
-        Mantente al tanto de las últimas noticias, eventos y desarrollos en nuestra universidad. Desde anuncios importantes hasta logros destacados de nuestros estudiantes y profesores.
+      Sigue de cerca los logros y desempeños de nuestros equipos deportivos. Entérate de los eventos deportivos, entrevistas con atletas destacados y análisis de los campeonatos universitarios.
+
       </p>
 
       <div className="body-actualidad">
         {
           sports?.map((sport, i) => (
             <div className="Card-actualidad" key={sport.id}>
+              <div className="news-number">{`News #${i + 1}`}</div>
               <h3 className="Card-actualidad-title">{sport.Title}</h3>
+              <img className='img-actualidad' src={sport.Pic} alt="" />
               <h4 className="Card-actualidad-Editor">{`By ${sport.Editor}`}</h4>
               <p className="Card-actualidad-body">{sport.Body}</p>
               <h5 className="Card-actualidad-Date">{sport.Date}</h5>
--- src/Hooks/useSport.js
+++ src/Hooks/useSport.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 const useSport = () => {
     const [sports, setSport] = useState()
     useEffect(() => {
-        const URL = 'https://script.google.com/macros/s/AKfycbzBjeIQjh2-tFIYrbC7DoUVVRoqmYLToMIRVnpAXmV_vRV6YZKzANoYDKldGvKHRIT4mA/exec?action=data'
+        const URL = 'https://script.google.com/macros/s/AKfycbz1Ou5OyJLkHvj2q9mUm7fJzm6s2udphvnhFrqNBEscSpiCmwXFAVsG8XB4nAKcdN4pwQ/exec?action=data'
         axios.get(URL)
             .then(({ data }) => setSport(data))
             .catch(err => console.log(err))
