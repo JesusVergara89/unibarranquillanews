13974a6e098f2437f4966083950de397714892cd
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-25 12:50:10 +0100

    cultures componenst

--- src/Components/Travel.jsx
+++ src/Components/Travel.jsx
@@ -4,7 +4,7 @@ import useCulture from '../Hooks/UseCulture'
 
 const Travel = () => {
 
-  const culture =  useCulture()
+  const culture = useCulture()
 
   console.log(culture)
 
@@ -12,17 +12,19 @@ const Travel = () => {
     <article className="engineering_section">
       <div className="visualization-div-header"></div>
       <h2 className="title-actualidad">
-      CULTURA Y ARTE
+        CULTURA Y ARTE
       </h2>
       <p className='description-actualidad'>
-      Explora la escena cultural y artística en Unibarranquilla. Reseñas de eventos, entrevistas con artistas locales y destacados, así como la cobertura de actividades culturales organizadas por la universidad.
+        Explora la escena cultural y artística en Unibarranquilla. Reseñas de eventos, entrevistas con artistas locales y destacados, así como la cobertura de actividades culturales organizadas por la universidad.
       </p>
 
       <div className="body-actualidad">
         {
           culture?.map((cult, i) => (
             <div className="Card-actualidad" key={cult.id}>
+              <div className="news-number">{`News #${i + 1}`}</div>
               <h3 className="Card-actualidad-title">{cult.Title}</h3>
+              <img className='img-actualidad' src={cult.Pic} alt="" />
               <h4 className="Card-actualidad-Editor">{`By ${cult.Editor}`}</h4>
               <p className="Card-actualidad-body">{cult.Body}</p>
               <h5 className="Card-actualidad-Date">{cult.Date}</h5>
--- src/Hooks/UseCulture.js
+++ src/Hooks/UseCulture.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 const useCulture = () => {
     const [culture, setCulture] = useState()
     useEffect(() => {
-        const URL = 'https://script.google.com/macros/s/AKfycbxSx1jAhGSDdDsqQsqRdHmKjCy1ZKRiI_UFI7l47t2GNX8uDs71N7KbqW2mQMa7jNufjg/exec?action=data'
+        const URL = 'https://script.google.com/macros/s/AKfycbymRjFy-ioSfC5YjSC87S2LID9CfLhkZgOGBdj6oynHghmqLlb0XYqUqvl5F2OLJuDM4g/exec?action=data'
         axios.get(URL)
             .then(({ data }) => setCulture(data))
             .catch(err => console.log(err))
