8a5f0d9fb417e65d8881907c9f976d94a8e48928
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-27 21:52:06 +0100

    social media uldate and index all unset of ancores

--- src/Components/Socialmedia.jsx
+++ src/Components/Socialmedia.jsx
@@ -3,9 +3,23 @@ import '../Styles/Socialmedia.css'
 
 const Socialmedia = () => {
   return (
-    <footer>
-        <i className='bx bxl-instagram'></i>
-    </footer>
+<footer>
+  <div className="footer-inter">
+    <a href="https://www.instagram.com" target="_blank">
+      <i className='bx bxl-instagram-alt'></i>
+    </a>
+    <a href="https://www.facebook.com" target="_blank">
+      <i className='bx bxl-facebook'></i>
+    </a>
+    <a href="https://twitter.com" target="_blank">
+      <i className='bx bxl-twitter'></i>
+    </a>
+    <a href="mailto:tuemail@example.com" target="_blank">
+      <i className='bx bxl-gmail'></i>
+    </a>
+  </div>
+</footer>
+
   )
 }
 
