917673b1328dc95b4557ca23a578b41018f66c48
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-27 20:48:25 +0100

    uldate literature components

--- src/Components/Literature.jsx
+++ src/Components/Literature.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styles/Literature.css'
 import useSport from '../Hooks/useSport'
+import Loading from './Loading'
 
 const Literature = () => {
 
@@ -15,24 +16,45 @@ const Literature = () => {
         DEPORTES UNIVERSITARIOS
       </h2>
       <p className='description-actualidad'>
-      Sigue de cerca los logros y desempeños de nuestros equipos deportivos. Entérate de los eventos deportivos, entrevistas con atletas destacados y análisis de los campeonatos universitarios.
+        Sigue de cerca los logros y desempeños de nuestros equipos deportivos. Entérate de los eventos deportivos, entrevistas con atletas destacados y análisis de los campeonatos universitarios.
 
       </p>
 
-      <div className="body-actualidad">
-        {
-          sports?.map((sport, i) => (
-            <div className="Card-actualidad" key={sport.id}>
-              <div className="news-number">{`News #${i + 1}`}</div>
-              <h3 className="Card-actualidad-title">{sport.Title}</h3>
-              <img className='img-actualidad' src={sport.Pic} alt="" />
-              <h4 className="Card-actualidad-Editor">{`By ${sport.Editor}`}</h4>
-              <p className="Card-actualidad-body">{sport.Body}</p>
-              <h5 className="Card-actualidad-Date">{sport.Date}</h5>
-            </div>
-          ))
-        }
-      </div>
+      {sports ? (
+        <div className="body-actualidad">
+          {
+            sports?.map((sport, i) => (
+              <div className="Card-actualidad" key={sport.id}>
+                <div className="news-number">{`News #${i + 1}`}</div>
+                <h3 className="Card-actualidad-title">{sport.Title}</h3>
+                <img className='img-actualidad' src={sport.Pic} alt="" />
+                <h4 className="Card-actualidad-Editor">{`By ${sport.Editor}`}</h4>
+                <p className="Card-actualidad-body">{sport.Body}</p>
+                <h5 className="Card-actualidad-Date">{sport.Date}</h5>
+                <div className='Card-social'>
+                  <h5>Comparte:</h5>
+                  <div className="Card-social-btn">
+                    <button
+                      onClick={() => {
+                        const url = `https://www.facebook.com/sharer/sharer.php?u=https://bit.ly/3SzyQkU'`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-facebook-square'></i></button>
+                    <button
+                      onClick={() => {
+                        const truncatedText = sport.Title.slice(0, 50);
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-twitter' ></i></button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
     </article>
   )
 }
