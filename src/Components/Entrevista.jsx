faa9ec2ff98c71848788302d979427181fe5cde8
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-27 20:39:28 +0100

    update entrevista compoennts

--- src/Components/Entrevista.jsx
+++ src/Components/Entrevista.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styles/Entrevista.css'
 import useEntrevista from '../Hooks/useEntrevista'
+import Loading from './Loading'
 
 const Entrevista = () => {
 
@@ -18,20 +19,41 @@ const Entrevista = () => {
         Mantente al tanto de las últimas noticias, eventos y desarrollos en nuestra universidad. Desde anuncios importantes hasta logros destacados de nuestros estudiantes y profesores.
       </p>
 
-      <div className="body-actualidad">
-        {
-          entrevista?.map((entrev, i) => (
-            <div className="Card-actualidad" key={entrev.id}>
-              <div className="news-number">{`News #${i + 1}`}</div>
-              <h3 className="Card-actualidad-title">{entrev.Title}</h3>
-              <img className='img-actualidad' src={entrev.Pic} alt="" />
-              <h4 className="Card-actualidad-Editor">{`By ${entrev.Editor}`}</h4>
-              <p className="Card-actualidad-body">{entrev.Body}</p>
-              <h5 className="Card-actualidad-Date">{entrev.Date}</h5>
-            </div>
-          ))
-        }
-      </div>
+      {entrevista ? (
+        <div className="body-actualidad">
+          {
+            entrevista?.map((entrev, i) => (
+              <div className="Card-actualidad" key={entrev.id}>
+                <div className="news-number">{`News #${i + 1}`}</div>
+                <h3 className="Card-actualidad-title">{entrev.Title}</h3>
+                <img className='img-actualidad' src={entrev.Pic} alt="" />
+                <h4 className="Card-actualidad-Editor">{`By ${entrev.Editor}`}</h4>
+                <p className="Card-actualidad-body">{entrev.Body}</p>
+                <h5 className="Card-actualidad-Date">{entrev.Date}</h5>
+                <div className='Card-social'>
+                  <h5>Comparte:</h5>
+                  <div className="Card-social-btn">
+                    <button
+                      onClick={() => {
+                        const url = `https://www.facebook.com/sharer/sharer.php?u=https://bit.ly/3SzyQkU'`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-facebook-square'></i></button>
+                    <button
+                      onClick={() => {
+                        const truncatedText = entrev.Title.slice(0, 50);
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-twitter' ></i></button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
     </article>
   )
 }
