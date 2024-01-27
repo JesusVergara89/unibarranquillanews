3d71d2b5c0b6034f0ab23882b59d69e98d206b38
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-27 21:06:24 +0100

    update experience

--- src/Components/Travel.jsx
+++ src/Components/Travel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Styles/Travel.css'
 import useCulture from '../Hooks/UseCulture'
+import Loading from './Loading'
 
 const Travel = () => {
 
@@ -18,20 +19,41 @@ const Travel = () => {
         Explora la escena cultural y artística en Unibarranquilla. Reseñas de eventos, entrevistas con artistas locales y destacados, así como la cobertura de actividades culturales organizadas por la universidad.
       </p>
 
-      <div className="body-actualidad">
-        {
-          culture?.map((cult, i) => (
-            <div className="Card-actualidad" key={cult.id}>
-              <div className="news-number">{`News #${i + 1}`}</div>
-              <h3 className="Card-actualidad-title">{cult.Title}</h3>
-              <img className='img-actualidad' src={cult.Pic} alt="" />
-              <h4 className="Card-actualidad-Editor">{`By ${cult.Editor}`}</h4>
-              <p className="Card-actualidad-body">{cult.Body}</p>
-              <h5 className="Card-actualidad-Date">{cult.Date}</h5>
-            </div>
-          ))
-        }
-      </div>
+      {culture ? (
+        <div className="body-actualidad">
+          {
+            culture?.map((cult, i) => (
+              <div className="Card-actualidad" key={cult.id}>
+                <div className="news-number">{`News #${i + 1}`}</div>
+                <h3 className="Card-actualidad-title">{cult.Title}</h3>
+                <img className='img-actualidad' src={cult.Pic} alt="" />
+                <h4 className="Card-actualidad-Editor">{`By ${cult.Editor}`}</h4>
+                <p className="Card-actualidad-body">{cult.Body}</p>
+                <h5 className="Card-actualidad-Date">{cult.Date}</h5>
+                <div className='Card-social'>
+                  <h5>Comparte:</h5>
+                  <div className="Card-social-btn">
+                    <button
+                      onClick={() => {
+                        const url = `https://www.facebook.com/sharer/sharer.php?u=https://bit.ly/3SzyQkU'`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-facebook-square'></i></button>
+                    <button
+                      onClick={() => {
+                        const truncatedText = cult.Title.slice(0, 50);
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-twitter' ></i></button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
     </article>
   )
 }
