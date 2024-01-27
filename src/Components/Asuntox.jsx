5a78af9893d89474ef2faa3f3e891878b2b32eb7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-27 20:32:01 +0100

    update asuntos

--- src/Components/Asuntox.jsx
+++ src/Components/Asuntox.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useAsuntos from '../Hooks/useAsuntos'
 import '../Styles/Asuntosx.css'
+import Loading from './Loading'
 
 const Asuntox = () => {
 
@@ -13,23 +14,44 @@ const Asuntox = () => {
                 ASUNTOS UNIVERSITARIOS
             </h2>
             <p className='description-actualidad'>
-            Enfocándonos en las preocupaciones y necesidades de los estudiantes, abordaremos temas relevantes como becas, orientación académica, oportunidades de empleo y todo aquello que impacta directamente en tu experiencia universitaria.
+                Enfocándonos en las preocupaciones y necesidades de los estudiantes, abordaremos temas relevantes como becas, orientación académica, oportunidades de empleo y todo aquello que impacta directamente en tu experiencia universitaria.
             </p>
 
-            <div className="body-actualidad">
-                {
-                    asuntos?.map((asunto, i) => (
-                        <div className="Card-actualidad" key={asunto.id}>
-                            <div className="news-number">{`News #${i + 1}`}</div>
-                            <h3 className="Card-actualidad-title">{asunto.Title}</h3>
-                            <img className='img-actualidad' src={asunto.Pic} alt="" />
-                            <h4 className="Card-actualidad-Editor">{`By ${asunto.Editor}`}</h4>
-                            <p className="Card-actualidad-body">{asunto.Body}</p>
-                            <h5 className="Card-actualidad-Date">{asunto.Date}</h5>
-                        </div>
-                    ))
-                }
-            </div>
+            {asuntos ? (
+                <div className="body-actualidad">
+                    {
+                        asuntos?.map((asunto, i) => (
+                            <div className="Card-actualidad" key={i}>
+                                <div className="news-number">{`News #${i + 1}`}</div>
+                                <h3 className="Card-actualidad-title">{asunto.Title}</h3>
+                                <img className='img-actualidad' src={asunto.Pic} alt="" />
+                                <h4 className="Card-actualidad-Editor">{`By ${asunto.Editor}`}</h4>
+                                <p className="Card-actualidad-body">{asunto.Body}</p>
+                                <h5 className="Card-actualidad-Date">{asunto.Date}</h5>
+                                <div className='Card-social'>
+                                    <h5>Comparte:</h5>
+                                    <div className="Card-social-btn">
+                                        <button
+                                            onClick={() => {
+                                                const url = `https://www.facebook.com/sharer/sharer.php?u=https://bit.ly/3SzyQkU'`;
+                                                window.open(url, '_blank');
+                                            }}
+                                        ><i className='bx bxl-facebook-square'></i></button>
+                                        <button
+                                            onClick={() => {
+                                                const truncatedText = asunto.Title.slice(0, 50);
+                                                const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                                                window.open(url, '_blank');
+                                            }}
+                                        ><i className='bx bxl-twitter' ></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            ) : (
+                <Loading />
+            )}
         </article>
     )
 }
