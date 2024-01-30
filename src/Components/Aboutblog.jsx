deb898afe0806e0b03ba5000cf05af7b5bd65890
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-01-30 16:05:24 +0100

    i dont know

--- src/Components/Aboutblog.jsx
+++ src/Components/Aboutblog.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import '../Styles/Aboutblog.css'
+import '../Styles/Aboutblog.css';
 
 const Aboutblog = () => {
     return (
         <article className='About-this-blog'>
 
             <p>
-            ¡Bienvenidos a la plataforma digital independiente de noticias de la Universidad de Barranquilla!
+                ¡Bienvenidos a la plataforma digital independiente de noticias de la Universidad de Barranquilla!
             </p>
 
             <a href="https://www.unibarranquilla.edu.co/" target="_blank">Página oficial de la universidad</a>
 
             <p>
-            En Unibarranquilla Noticias, estamos emocionados de ser tu fuente de información y actualización constante sobre todo lo que acontece en nuestra vibrante comunidad universitaria. Aquí encontrarás una variedad de temas que reflejan la diversidad de intereses y actividades que enriquecen nuestra vida académica y estudiantil. ¡Explora, descubre y mantente conectado!
+                En Unibarranquilla Noticias, estamos emocionados de ser tu fuente de información y actualización constante sobre todo lo que acontece en nuestra vibrante comunidad universitaria. Aquí encontrarás una variedad de temas que reflejan la diversidad de intereses y actividades que enriquecen nuestra vida académica y estudiantil. ¡Explora, descubre y mantente conectado!
             </p>
 
             <p>
                 Nuestras secciones
-            </p>         
+            </p>
 
         </article>
-    )
-}
+    );
+};
 
-export default Aboutblog
+export default Aboutblog;
