967bd31062432fb732ad96a98b5beb9d04c27173
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 11:48:14 +0100

    src/Components/Networks.jsx src/Components/Presentations.jsx  src/Components/Recent.jsx

--- src/Components/Recent.jsx
+++ src/Components/Recent.jsx
@@ -13,9 +13,15 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+/**
+ * Component displaying the most recent articles from various sections.
+ * Uses sliders to showcase the articles.
+ * @returns {JSX.Element} Recent component JSX
+ */
 const Recent = () => {
     const [data, setData] = useState(null);
 
+    // Custom hooks to fetch data for various sections
     const actualidad = useActualidad();
     const entrevista = useEntrevista();
     const eventos = useEventos();
@@ -25,6 +31,7 @@ const Recent = () => {
     const vidas = useVida();
     const asuntos = useAsuntos();
 
+    // Settings for the slider
     const settings = {
         dots: false,
         infinite: true,
@@ -60,10 +67,12 @@ const Recent = () => {
         ]
     };
 
+    // Function to open the article URL in a new tab
     const openUrl = (url) => {
         window.open(url, '_blank');
     };
 
+    // useEffect to set data once all data from different sections are fetched
     useEffect(() => {
         if (actualidad && asuntos && entrevista && eventos && researchs && culture && sports && vidas) {
             setData([actualidad[0], asuntos[0], entrevista[0], eventos[0], researchs[0], culture[0], sports[0], vidas[0]]);
@@ -72,7 +81,7 @@ const Recent = () => {
 
     return (
         <article className="Recent-news">
-            <h2>Artículos más recientes</h2>
+            <h2>Most Recent Articles</h2>
             {data ? (
                 <Slider className="recent-news-slider" {...settings}>
                     {data.map((unit, i) => (
@@ -80,7 +89,7 @@ const Recent = () => {
                             <h3 className="recent-news-title">{unit.Title.slice(0, 60)}</h3>
                             <img src={unit.Pic} alt="" className="recent-news-img" />
                             <button onClick={() => openUrl(unit.interLink)} className="recen-btn">
-                                <a href={unit.interLink} target="_blank" rel="noopener noreferrer">ver</a>
+                                <a href={unit.interLink} target="_blank" rel="noopener noreferrer">view</a>
                             </button>
                         </div>
                     ))}
@@ -90,7 +99,7 @@ const Recent = () => {
             )}
 
             <h4>
-                Nuestras secciones
+                Our Sections
             </h4>
         </article>
     );
