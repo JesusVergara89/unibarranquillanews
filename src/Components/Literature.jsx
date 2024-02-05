05feb19bccc3ea452036398ad8b3a6c38900ae5b
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 11:38:50 +0100

    commenting literature and loading project

--- src/Components/Literature.jsx
+++ src/Components/Literature.jsx
@@ -1,67 +1,76 @@
-import React from 'react'
-import useSport from '../Hooks/useSport'
-import Loading from './Loading'
+import React from 'react';
+import useSport from '../Hooks/useSport';
+import Loading from './Loading';
 
+/**
+ * Component displaying information about university sports.
+ * Fetches sports data using the useSport hook.
+ * @returns {JSX.Element} Component JSX
+ */
 const Literature = () => {
-
-  const sports = useSport()
-
-  const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/LITERATURE'
+  // Fetch sports data
+  const sports = useSport();
+  // Current URL of the component
+  const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/LITERATURE';
 
   return (
     <article className="engineering_section">
       <div className="visualization-div-header"></div>
       <h2 className="title-actualidad">
-        DEPORTES UNIVERSITARIOS
+        UNIVERSITY SPORTS
       </h2>
       <p className='description-actualidad'>
-        Sigue de cerca los logros y desempeños de nuestros equipos deportivos. Entérate de los eventos deportivos, entrevistas con atletas destacados y análisis de los campeonatos universitarios.
-
+        Follow closely the achievements and performances of our sports teams. Learn about sports events, interviews with standout athletes, and analysis of university championships.
       </p>
 
+      {/* Check if sports data is available */}
       {sports ? (
         <div className="body-actualidad">
-          {
-            sports?.map((sport, i) => (
-              <div className="Card-actualidad" key={sport.id}>
-                <div className="news-number">{`News #${i + 1}`}</div>
-                <h3 className="Card-actualidad-title">{sport.Title}</h3>
-                <img className='img-actualidad' src={sport.Pic} alt="" />
-                <h4 className="Card-actualidad-Editor">{`By ${sport.Editor}`}</h4>
-                
-                <p className="Card-actualidad-body">
-                  {sport.Body.split('\n').map((line, index) => (
-                    <p key={index}>{line}</p>
-                  ))}
-                </p>
-                <h5 className="Card-actualidad-Date">{sport.Date}</h5>
-                <button className="Card-link"> <a href={sport.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
-                <div className='Card-social'>
-                  <h5>Comparte:</h5>
-                  <div className="Card-social-btn">
-                    <button
-                      onClick={() => {
-                        const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
-                        window.open(url, '_blank');
-                      }}
-                    ><i className='bx bxl-facebook-square'></i></button>
-                    <button
-                      onClick={() => {
-                        const truncatedText = sport.Title.slice(0, 50);
-                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
-                        window.open(url, '_blank');
-                      }}
-                    ><i className='bx bxl-twitter' ></i></button>
-                  </div>
+          {/* Map through sports data and render cards */}
+          {sports?.map((sport, i) => (
+            <div className="Card-actualidad" key={sport.id}>
+              <div className="news-number">{`News #${i + 1}`}</div>
+              <h3 className="Card-actualidad-title">{sport.Title}</h3>
+              <img className='img-actualidad' src={sport.Pic} alt="" />
+              <h4 className="Card-actualidad-Editor">{`By ${sport.Editor}`}</h4>
+
+              <p className="Card-actualidad-body">
+                {/* Split body text by new line and map through */}
+                {sport.Body.split('\n').map((line, index) => (
+                  <p key={index}>{line}</p>
+                ))}
+              </p>
+              <h5 className="Card-actualidad-Date">{sport.Date}</h5>
+              {/* Button to view more details */}
+              <button className="Card-link"> <a href={sport.Link} target="_blank" rel="noopener noreferrer">Read more</a> </button>
+              {/* Social media sharing buttons */}
+              <div className='Card-social'>
+                <h5>Share:</h5>
+                <div className="Card-social-btn">
+                  <button
+                    onClick={() => {
+                      const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
+                      window.open(url, '_blank');
+                    }}
+                  ><i className='bx bxl-facebook-square'></i></button>
+                  <button
+                    onClick={() => {
+                      const truncatedText = sport.Title.slice(0, 50);
+                      const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                      window.open(url, '_blank');
+                    }}
+                  ><i className='bx bxl-twitter' ></i></button>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       ) : (
+        // Display loading component while fetching data
         <Loading />
       )}
     </article>
-  )
-}
+  );
+};
 
-export default Literature
+export default Literature;
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { lineSpinner } from 'ldrs';
+import { lineSpinner } from 'ldrs'; // Importing lineSpinner from ldrs package
 
+/**
+ * Component displaying a loading spinner.
+ * Uses the ldrs package to render a line spinner.
+ * @returns {JSX.Element} Component JSX
+ */
 const Loading = () => {
-
-    const newColor = '#111111'
+    // Define new color for the spinner
+    const newColor = '#111111';
+    // Register lineSpinner component
     lineSpinner.register();
+
     return (
         <div className="loading-container">
+            {/* Render lineSpinner component with specified props */}
             <l-line-spinner
                 size="100"
                 stroke="7"
@@ -18,3 +26,4 @@ const Loading = () => {
 };
 
 export default Loading;
+
