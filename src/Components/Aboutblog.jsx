c20130e6c9bc9d73141ea5f8a42ac9fa02eb9fbc
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 10:54:06 +0100

    documenting aboutblog and engineering

--- src/Components/Aboutblog.jsx
+++ src/Components/Aboutblog.jsx
@@ -1,15 +1,22 @@
 import '../Styles/Aboutblog.css';
 
+/**
+ * Functional component representing the about section of the blog.
+ * It provides a brief introduction to the platform and its purpose.
+ */
 const Aboutblog = () => {
     return (
         <article className='About-this-blog'>
 
+            {/* Introduction paragraph */}
             <p>
                 ¡Bienvenidos a la plataforma digital independiente de noticias de la Universidad de Barranquilla!
             </p>
 
+            {/* Link to official university website */}
             <a href="https://www.unibarranquilla.edu.co/" target="_blank">Página oficial de la universidad</a>
 
+            {/* Description paragraph */}
             <p>
                 En Unibarranquilla Noticias, estamos emocionados de ser tu fuente de información y actualización constante sobre todo lo que acontece en nuestra vibrante comunidad universitaria. Aquí encontrarás una variedad de temas que reflejan la diversidad de intereses y actividades que enriquecen nuestra vida académica y estudiantil. ¡Explora, descubre y mantente conectado!
             </p>
--- src/Components/Engineering.jsx
+++ src/Components/Engineering.jsx
@@ -1,11 +1,16 @@
 import useActualidad from '../Hooks/useActualidad'
 import Loading from './Loading'
 
-
+/**
+ * Functional component representing the Engineering section of the blog.
+ * It displays updates, news, and developments related to university life.
+ */
 const Engineering = () => {
-
+  // Custom hook to fetch updates
   const update = useActualidad()
+  // Current URL of the page
   const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ENGINEERING'
+
   return (
     <article className="engineering_section">
       <div className="visualization-div-header"></div>
@@ -13,38 +18,46 @@ const Engineering = () => {
         ACTUALIDAD UNIVERSITARIA
       </h2>
       <p className='description-actualidad'>
-        Mantente al tanto de las últimas noticias, eventos y desarrollos en nuestra universidad. Desde anuncios importantes hasta logros destacados de nuestros estudiantes y profesores.
+        Stay informed about the latest news, events, and developments at our university. From important announcements to notable achievements of our students and faculty.
       </p>
 
+      {/* Display updates if available, otherwise show loading indicator */}
       {update ? (
         <div className="body-actualidad">
+          {/* Map through updates */}
           {update?.map((news, i) => (
             <div className="Card-actualidad" key={i}>
               <div className="news-number">{`News #${i + 1}`}</div>
               <h3 className="Card-actualidad-title">{news.Title}</h3>
               <img className='img-actualidad' src={news.Pic} alt="" />
               <h4 className="Card-actualidad-Editor">{`By ${news.Editor}`}</h4>
-              
+
               <p className="Card-actualidad-body">
+                {/* Split body content by newline and display */}
                 {news.Body.split('\n').map((line, index) => (
                   <p key={index}>{line}</p>
                 ))}
               </p>
               <h5 className="Card-actualidad-Date">{news.Date}</h5>
+              {/* Button to view full article */}
               <button className="Card-link"> <a href={news.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
+              {/* Social sharing options */}
               <div className='Card-social'>
                 <h5>Comparte:</h5>
                 <div className="Card-social-btn">
+                  {/* Share on Facebook */}
                   <button
                     onClick={() => {
                       const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
                       window.open(url, '_blank');
                     }}
                   ><i className='bx bxl-facebook-square'></i></button>
+                  {/* Share on Twitter */}
                   <button
                     onClick={() => {
+                      // Truncate title for Twitter sharing
                       const truncatedText = news.Title.slice(0, 50);
-                      const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                      const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
                       window.open(url, '_blank');
                     }}
                   ><i className='bx bxl-twitter' ></i></button>
@@ -54,6 +67,7 @@ const Engineering = () => {
           ))}
         </div>
       ) : (
+        // Show loading indicator if updates are being fetched
         <Loading />
       )}
     </article>
