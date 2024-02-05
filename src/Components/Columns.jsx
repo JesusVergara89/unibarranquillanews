3a4bcc1ff33435731e505b2d772f06d671f27809
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 20:47:45 +0100

    updating columns

--- src/Components/Columns.jsx
+++ src/Components/Columns.jsx
@@ -24,49 +24,49 @@ const Columns = () => {
     return (
         <div className="container">
             <button className='outside-btn' onClick={() => navigateEngi('/ENGINEERING')}  >
-                <img onClick={() => navigateEngi('/ENGINEERING')} src="https://photo-data-base.netlify.app/assets/people.dd634ce8.jpg" />
+                <img onClick={() => navigateEngi('/ENGINEERING')} src="https://photo-data-base.netlify.app/assets/people.e4fef31b.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Actualidad Universitaria</h3>
             </button>
 
             <button id='outside-btn' onClick={() => navigateTravel('/TRAVEL')}  >
-                <img onClick={() => navigateTravel('/TRAVEL')} src="https://photo-data-base.netlify.app/assets/arte.be73a92a.jpg" />
+                <img onClick={() => navigateTravel('/TRAVEL')} src="https://photo-data-base.netlify.app/assets/arte.a47a54ae.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Cultura y Arte</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateLiterature('/LITERATURE')} >
-                <img onClick={() => navigateLiterature('/LITERATURE')} src="https://photo-data-base.netlify.app/assets/deporte.e5110516.jpg" />
+                <img onClick={() => navigateLiterature('/LITERATURE')} src="https://photo-data-base.netlify.app/assets/deporte.01eed798.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Deportes Universitarios</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateExperience('/EXPERIENCE')} >
-                <img onClick={() => navigateExperience('/EXPERIENCE')} src="https://photo-data-base.netlify.app/assets/microscope.f118c0e4.jpg" />
+                <img onClick={() => navigateExperience('/EXPERIENCE')} src="https://photo-data-base.netlify.app/assets/microscope.39879e23.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Investigación y Desarrollo</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateAsuntos('/ASUNTOS')} >
-                <img onClick={() => navigateAsuntos('/ASUNTOS')} src="https://photo-data-base.netlify.app/assets/city.850437a4.jpg" />
+                <img onClick={() => navigateAsuntos('/ASUNTOS')} src="https://photo-data-base.netlify.app/assets/city.7f4b3f45.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Mi universidad, mi ciudad</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateVida('/VIDA')} >
-                <img onClick={() => navigateVida('/VIDA')} src="https://photo-data-base.netlify.app/assets/estudiantes.a90aad50.jpg" />
+                <img onClick={() => navigateVida('/VIDA')} src="https://photo-data-base.netlify.app/assets/estudiantes.1638c5fc.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Vida Estudiantil</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateEvento('/EVENTO')} >
-                <img onClick={() => navigateEvento('/EVENTO')} src="https://photo-data-base.netlify.app/assets/evento.b25824db.jpg" />
+                <img onClick={() => navigateEvento('/EVENTO')} src="https://photo-data-base.netlify.app/assets/evento.99182bf6.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Eventos y Actividades</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateEntrevista('/ENTREVISTA')} >
-                <img  onClick={() => navigateEntrevista('/ENTREVISTA')} src="https://photo-data-base.netlify.app/assets/entrevista.6f892194.jpg" />
+                <img  onClick={() => navigateEntrevista('/ENTREVISTA')} src="https://photo-data-base.netlify.app/assets/entrevista.edf8a3c4.jpg" />
                 <h2 >Ver más</h2>
                 <h3>Entrevistas y Perfiles</h3>
             </button>
