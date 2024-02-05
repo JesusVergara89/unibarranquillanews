1c05395a29b87dc087b2db82654cb49f5f9d4d59
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 21:35:50 +0100

    new rediret de carousel

--- src/Components/Recent.jsx
+++ src/Components/Recent.jsx
@@ -12,6 +12,7 @@ import '../Styles/Recen.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import useTecnologia from '../Hooks/useTecnologia';
 
 const Recent = () => {
     const [data, setData] = useState(null);
@@ -24,12 +25,13 @@ const Recent = () => {
     const sports = useSport();
     const vidas = useVida();
     const asuntos = useAsuntos();
+    const tecnologias = useTecnologia()
 
     const settings = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         vertical: false,
         verticalSwiping: false,
@@ -65,10 +67,10 @@ const Recent = () => {
     };
 
     useEffect(() => {
-        if (actualidad && asuntos && entrevista && eventos && researchs && culture && sports && vidas) {
-            setData([actualidad[0], asuntos[0], entrevista[0], eventos[0], researchs[0], culture[0], sports[0], vidas[0]]);
+        if (actualidad && asuntos && entrevista && eventos && researchs && culture && sports && vidas && tecnologias) {
+            setData([actualidad[0], asuntos[0], entrevista[0], eventos[0], researchs[0], culture[0], sports[0], vidas[0], tecnologias[0]]);
         }
-    }, [actualidad, asuntos, entrevista, eventos, researchs, culture, sports, vidas]);
+    }, [actualidad, asuntos, entrevista, eventos, researchs, culture, sports, vidas, tecnologias]);
 
     return (
         <article className="Recent-news">
