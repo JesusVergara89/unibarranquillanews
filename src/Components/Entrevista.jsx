b7769b57d58f9293768eb5038707610f7172a9a0
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-05 11:02:18 +0100

    m¿commeting entrevista and eventos components

--- src/Components/Entrevista.jsx
+++ src/Components/Entrevista.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import useEntrevista from '../Hooks/useEntrevista'
 import Loading from './Loading'
 
+/**
+ * Functional component representing the Interviews and Profiles section of the blog.
+ * It showcases in-depth interviews with leaders, academics, and personalities within the university community.
+ */
 const Entrevista = () => {
-
+  // Custom hook to fetch interviews
   const entrevista = useEntrevista()
-
+  // Current URL of the page
   const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ENTREVISTA'
 
   return (
@@ -15,11 +19,13 @@ const Entrevista = () => {
         ENTREVISTAS Y PERFILES
       </h2>
       <p className='description-actualidad'>
-        Conoce a fondo a los líderes, académicos y personalidades que forman parte de nuestra comunidad universitaria. Descubre sus historias, experiencias y contribuciones a la educación superior.
+        Get to know the leaders, academics, and personalities who are part of our university community. Discover their stories, experiences, and contributions to higher education.
       </p>
 
+      {/* Display interviews if available, otherwise show loading indicator */}
       {entrevista ? (
         <div className="body-actualidad">
+          {/* Map through interviews */}
           {
             entrevista?.map((entrev, i) => (
               <div className="Card-actualidad" key={entrev.id}>
@@ -27,27 +33,33 @@ const Entrevista = () => {
                 <h3 className="Card-actualidad-title">{entrev.Title}</h3>
                 <img className='img-actualidad' src={entrev.Pic} alt="" />
                 <h4 className="Card-actualidad-Editor">{`By ${entrev.Editor}`}</h4>
-                
+
                 <p className="Card-actualidad-body">
-                {entrev.Body.split('\n').map((line, index) => (
-                  <p key={index}>{line}</p>
-                ))}
-              </p>
+                  {/* Split body content by newline and display */}
+                  {entrev.Body.split('\n').map((line, index) => (
+                    <p key={index}>{line}</p>
+                  ))}
+                </p>
                 <h5 className="Card-actualidad-Date">{entrev.Date}</h5>
+                {/* Button to view full article */}
                 <button className="Card-link"> <a href={entrev.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
+                {/* Social sharing options */}
                 <div className='Card-social'>
                   <h5>Comparte:</h5>
                   <div className="Card-social-btn">
+                    {/* Share on Facebook */}
                     <button
                       onClick={() => {
                         const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
                         window.open(url, '_blank');
                       }}
                     ><i className='bx bxl-facebook-square'></i></button>
+                    {/* Share on Twitter */}
                     <button
                       onClick={() => {
+                        // Truncate title for Twitter sharing
                         const truncatedText = entrev.Title.slice(0, 50);
-                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
                         window.open(url, '_blank');
                       }}
                     ><i className='bx bxl-twitter' ></i></button>
@@ -57,6 +69,7 @@ const Entrevista = () => {
             ))}
         </div>
       ) : (
+        // Show loading indicator if interviews are being fetched
         <Loading />
       )}
     </article>
--- src/Components/Eventos.jsx
+++ src/Components/Eventos.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import useEventos from '../Hooks/useEventos'
 import Loading from './Loading'
 
+/**
+ * Functional component representing the Events and Activities section of the blog.
+ * It showcases upcoming events, conferences, seminars, and social activities that enrich university life.
+ */
 const Eventos = () => {
+  // Custom hook to fetch events
   const eventos = useEventos()
+  // Current URL of the page
   const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/EVENTO'
+
   return (
     <article className="engineering_section">
       <div className="visualization-div-header"></div>
@@ -12,39 +19,47 @@ const Eventos = () => {
         EVENTOS Y ACTIVIDADES
       </h2>
       <p className='description-actualidad'>
-        Entérate de los eventos próximos, conferencias, seminarios y actividades sociales que enriquecen nuestra vida universitaria. No te pierdas ninguna oportunidad de participar y ser parte activa de la comunidad.
+        Stay informed about upcoming events, conferences, seminars, and social activities that enrich our university life. Don't miss any opportunity to participate and be an active part of the community.
       </p>
 
+      {/* Display events if available, otherwise show loading indicator */}
       {eventos ? (
         <div className="body-actualidad">
+          {/* Map through events */}
           {
             eventos?.map((evento, i) => (
               <div className="Card-actualidad" key={evento.id}>
                 <div className="news-number">{`News #${i + 1}`}</div>
                 <h3 className="Card-actualidad-title">{evento.Title}</h3>
                 <img className='img-actualidad' src={evento.Pic} alt="" />
-                
+
                 <h4 className="Card-actualidad-Editor">{`By ${evento.Editor}`}</h4>
                 <p className="Card-actualidad-body">
+                  {/* Split body content by newline and display */}
                   {evento.Body.split('\n').map((line, index) => (
                     <p key={index}>{line}</p>
                   ))}
                 </p>
                 <h5 className="Card-actualidad-Date">{evento.Date}</h5>
+                {/* Button to view full article */}
                 <button className="Card-link"> <a href={evento.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
+                {/* Social sharing options */}
                 <div className='Card-social'>
                   <h5>Comparte:</h5>
                   <div className="Card-social-btn">
+                    {/* Share on Facebook */}
                     <button
                       onClick={() => {
                         const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
                         window.open(url, '_blank');
                       }}
                     ><i className='bx bxl-facebook-square'></i></button>
+                    {/* Share on Twitter */}
                     <button
                       onClick={() => {
+                        // Truncate title for Twitter sharing
                         const truncatedText = evento.Title.slice(0, 50);
-                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=https://bit.ly/3SzyQkU`;
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
                         window.open(url, '_blank');
                       }}
                     ><i className='bx bxl-twitter' ></i></button>
@@ -54,6 +69,7 @@ const Eventos = () => {
             ))}
         </div>
       ) : (
+        // Show loading indicator if events are being fetched
         <Loading />
       )}
     </article>
