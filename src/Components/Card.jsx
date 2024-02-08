7ee01cf1833865ed5a14f2fd8676f62f4d91f533
Brian Escorcia <naziegonzalez5@gmail.com>
2024-02-08 06:00:24 -0500

    Links repair

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import "../Styles/card.css"
 
 const Card = ({ unit }) => {
+    const openUrl = (url) => {
+        window.open(url, '_blank');
+    };
     return (
         <div onClick={() => openUrl(unit.interLink)} className="recent-news-card">
             <img src={unit.Pic} alt="" className="recent-news-img" />
-            <h3 className="recent-news-title">{unit.Title.slice(0, 55)+"..."}</h3>
+            <h3 className="recent-news-title">{unit.Title.slice(0, 55) + "..."}</h3>
             <button onClick={() => openUrl(unit.interLink)} className="recen-btn">
                 <a href={unit.interLink} target="_blank" rel="noopener noreferrer">ver</a>
             </button>
