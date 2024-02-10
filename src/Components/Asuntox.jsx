edae800e4350ed6a6d7ddd7c00a2eb4acb97d68c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-10 15:28:23 +0100

    pagination in component asunto

--- src/Components/Asuntox.jsx
+++ src/Components/Asuntox.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import useAsuntos from '../Hooks/useAsuntos'
-import Loading from './Loading'
+import React, { useState } from 'react';
+import useAsuntos from '../Hooks/useAsuntos';
+import Loading from './Loading';
 
-/**
- * Functional component representing the 'Asuntox' section.
- * It displays news articles related to university life and city events.
- */
 const Asuntox = () => {
-    // Custom hook to fetch news articles
-    const asuntos = useAsuntos()
-    // Current URL of the page
-    const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ASUNTOS'
+    const [paginaActual, setPaginaActual] = useState(1);
+    const elementosPorPagina = 3; // Cambia esto al número deseado de elementos por página
+
+    const asuntos = useAsuntos();
+    const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ASUNTOS';
+
+    // Calcular los índices de inicio y fin de los elementos a mostrar en la página actual
+    const indiceInicio = (paginaActual - 1) * elementosPorPagina;
+    const indiceFinal = paginaActual * elementosPorPagina;
+
+    // Manejar el cambio de página
+    const cambiarPagina = (nuevaPagina) => {
+        setPaginaActual(nuevaPagina);
+    };
 
     return (
         <article className="engineering_section">
@@ -19,60 +25,78 @@ const Asuntox = () => {
                 MI UNIVERSIDAD, MI CIUDAD
             </h2>
             <p className='description-actualidad'>
-            Esta sección ofrece una visión del vibrante intercambio entre la vida estudiantil y el pulso urbano. Desde destacados eventos académicos hasta iniciativas comunitarias, esta sección captura la esencia de cómo las instituciones educativas y sus entornos se complementan mutuamente, impactando tanto a los estudiantes como a los residentes locales.
+                Esta sección ofrece una visión del vibrante intercambio entre la vida estudiantil y el pulso urbano. Desde destacados eventos académicos hasta iniciativas comunitarias, esta sección captura la esencia de cómo las instituciones educativas y sus entornos se complementan mutuamente, impactando tanto a los estudiantes como a los residentes locales.
             </p>
 
             {/* Display news articles if available, otherwise show loading indicator */}
             {asuntos ? (
                 <div className="body-actualidad">
-                    {
-                        // Map through news articles
-                        asuntos?.map((asunto, i) => (
-                            <div className="Card-actualidad" key={i}>
-                                <div className="news-number">{`News #${i + 1}`}</div>
-                                <h3 className="Card-actualidad-title">{asunto.Title}</h3>
-                                <img className='img-actualidad' src={asunto.Pic} alt="" />
-                                <h4 className="Card-actualidad-Editor">{`By ${asunto.Editor}`}</h4>
-                                <p className="Card-actualidad-body">
-                                    {/* Split body content by newline and display */}
-                                    {asunto.Body.split('\n').map((line, index) => (
-                                        <p key={index}>{line}</p>
-                                    ))}
-                                </p>
-                                <h5 className="Card-actualidad-Date">{asunto.Date}</h5>
-                                {/* Button to view full article */}
-                                <button className="Card-link"> <a href={asunto.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
-                                {/* Social sharing options */}
-                                <div className='Card-social'>
-                                    <h5>Comparte:</h5>
-                                    <div className="Card-social-btn">
-                                        {/* Share on Facebook */}
-                                        <button
-                                            onClick={() => {
-                                                const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
-                                                window.open(url, '_blank');
-                                            }}
-                                        ><i className='bx bxl-facebook-square'></i></button>
-                                        {/* Share on Twitter */}
-                                        <button
-                                            onClick={() => {
-                                                // Truncate title for Twitter sharing
-                                                const truncatedText = asunto.Title.slice(0, 50);
-                                                const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
-                                                window.open(url, '_blank');
-                                            }}
-                                        ><i className='bx bxl-twitter' ></i></button>
-                                    </div>
+                    <div className="pagination-controls">
+                        <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
+                            Anterior
+                        </button>
+                        <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
+                        <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= asuntos.length}>
+                            Siguiente
+                        </button>
+                    </div>
+                    {/* Mapear solo los elementos correspondientes a la página actual */}
+                    {asuntos.slice(indiceInicio, indiceFinal).map((asunto, i) => (
+                        <div className="Card-actualidad" key={i}>
+                            <div className="news-number">{`News #${indiceInicio + i + 1}`}</div>
+                            <h3 className="Card-actualidad-title">{asunto.Title}</h3>
+                            <img className='img-actualidad' src={asunto.Pic} alt="" />
+                            <h4 className="Card-actualidad-Editor">{`By ${asunto.Editor}`}</h4>
+                            <p className="Card-actualidad-body">
+                                {/* Split body content by newline and display */}
+                                {asunto.Body.split('\n').map((line, index) => (
+                                    <p key={index}>{line}</p>
+                                ))}
+                            </p>
+                            <h5 className="Card-actualidad-Date">{asunto.Date}</h5>
+                            {/* Button to view full article */}
+                            <button className="Card-link"> <a href={asunto.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
+                            {/* Social sharing options */}
+                            <div className='Card-social'>
+                                <h5>Comparte:</h5>
+                                <div className="Card-social-btn">
+                                    {/* Share on Facebook */}
+                                    <button
+                                        onClick={() => {
+                                            const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
+                                            window.open(url, '_blank');
+                                        }}
+                                    ><i className='bx bxl-facebook-square'></i></button>
+                                    {/* Share on Twitter */}
+                                    <button
+                                        onClick={() => {
+                                            // Truncate title for Twitter sharing
+                                            const truncatedText = asunto.Title.slice(0, 50);
+                                            const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
+                                            window.open(url, '_blank');
+                                        }}
+                                    ><i className='bx bxl-twitter' ></i></button>
                                 </div>
                             </div>
-                        ))}
+                        </div>
+                    ))}
+                    {/* Controles de paginación */}
+                    <div className="pagination-controls">
+                        <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
+                            Anterior
+                        </button>
+                        <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
+                        <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= asuntos.length}>
+                            Siguiente
+                        </button>
+                    </div>
                 </div>
             ) : (
                 // Show loading indicator if news articles are being fetched
                 <Loading />
             )}
         </article>
-    )
-}
+    );
+};
 
-export default Asuntox
+export default Asuntox;
--- src/Components/Engineering.jsx
+++ src/Components/Engineering.jsx
@@ -1,100 +1,100 @@
-import React, { useState } from 'react';
-import useActualidad from '../Hooks/useActualidad';
-import Loading from './Loading';
+  import React, { useState } from 'react';
+  import useActualidad from '../Hooks/useActualidad';
+  import Loading from './Loading';
 
-const Engineering = () => {
-  const [paginaActual, setPaginaActual] = useState(1);
-  const elementosPorPagina = 3; // Cambia esto al número deseado de elementos por página
+  const Engineering = () => {
+    const [paginaActual, setPaginaActual] = useState(1);
+    const elementosPorPagina = 3; // Cambia esto al número deseado de elementos por página
 
-  const update = useActualidad();
-  const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ENGINEERING';
+    const update = useActualidad();
+    const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ENGINEERING';
 
-  // Calcular los índices de inicio y fin de los elementos a mostrar en la página actual
-  const indiceInicio = (paginaActual - 1) * elementosPorPagina;
-  const indiceFinal = paginaActual * elementosPorPagina;
+    // Calcular los índices de inicio y fin de los elementos a mostrar en la página actual
+    const indiceInicio = (paginaActual - 1) * elementosPorPagina;
+    const indiceFinal = paginaActual * elementosPorPagina;
 
-  // Manejar el cambio de página
-  const cambiarPagina = (nuevaPagina) => {
-    setPaginaActual(nuevaPagina);
-  };
+    // Manejar el cambio de página
+    const cambiarPagina = (nuevaPagina) => {
+      setPaginaActual(nuevaPagina);
+    };
 
-  return (
-    <article className="engineering_section">
-      <div className="visualization-div-header"></div>
-      <h2 className="title-actualidad">ACTUALIDAD</h2>
-      <p className='description-actualidad'>
-        Mantente informado sobre las últimas noticias de nuestra universidad y del mundo. Desde importantes anuncios hasta destacados logros de nuestros estudiantes y profesores.
-      </p>
+    return (
+      <article className="engineering_section">
+        <div className="visualization-div-header"></div>
+        <h2 className="title-actualidad">ACTUALIDAD</h2>
+        <p className='description-actualidad'>
+          Mantente informado sobre las últimas noticias de nuestra universidad y del mundo. Desde importantes anuncios hasta destacados logros de nuestros estudiantes y profesores.
+        </p>
 
-      {update ? (
-        <div className="body-actualidad">
-          <div className="pagination-controls">
-            <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
-              Anterior
-            </button>
-            <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
-            <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= update.length}>
-              Siguiente
-            </button>
-          </div>
-          {/* Mapear solo los elementos correspondientes a la página actual */}
-          {update.slice(indiceInicio, indiceFinal).map((news, i) => (
-            <div className="Card-actualidad" key={i}>
-              <div className="news-number">{`News #${indiceInicio + i + 1}`}</div>
-              <h3 className="Card-actualidad-title">{news.Title}</h3>
-              <img className='img-actualidad' src={news.Pic} alt="" />
-              <h4 className="Card-actualidad-Editor">{`By ${news.Editor}`}</h4>
+        {update ? (
+          <div className="body-actualidad">
+            <div className="pagination-controls">
+              <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
+                Anterior
+              </button>
+              <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
+              <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= update.length}>
+                Siguiente
+              </button>
+            </div>
+            {/* Mapear solo los elementos correspondientes a la página actual */}
+            {update.slice(indiceInicio, indiceFinal).map((news, i) => (
+              <div className="Card-actualidad" key={i}>
+                <div className="news-number">{`News #${indiceInicio + i + 1}`}</div>
+                <h3 className="Card-actualidad-title">{news.Title}</h3>
+                <img className='img-actualidad' src={news.Pic} alt="" />
+                <h4 className="Card-actualidad-Editor">{`By ${news.Editor}`}</h4>
 
-              <p className="Card-actualidad-body">
-                {/* Split body content by newline and display */}
-                {news.Body.split('\n').map((line, index) => (
-                  <p key={index}>{line}</p>
-                ))}
-              </p>
-              <h5 className="Card-actualidad-Date">{news.Date}</h5>
-              {/* Button to view full article */}
-              <button className="Card-link"> <a href={news.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
-              {/* Social sharing options */}
-              <div className='Card-social'>
-                <h5>Comparte:</h5>
-                <div className="Card-social-btn">
-                  {/* Share on Facebook */}
-                  <button
-                    onClick={() => {
-                      const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
-                      window.open(url, '_blank');
-                    }}
-                  ><i className='bx bxl-facebook-square'></i></button>
-                  {/* Share on Twitter */}
-                  <button
-                    onClick={() => {
-                      // Truncate title for Twitter sharing
-                      const truncatedText = news.Title.slice(0, 50);
-                      const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
-                      window.open(url, '_blank');
-                    }}
-                  ><i className='bx bxl-twitter' ></i></button>
+                <p className="Card-actualidad-body">
+                  {/* Split body content by newline and display */}
+                  {news.Body.split('\n').map((line, index) => (
+                    <p key={index}>{line}</p>
+                  ))}
+                </p>
+                <h5 className="Card-actualidad-Date">{news.Date}</h5>
+                {/* Button to view full article */}
+                <button className="Card-link"> <a href={news.Link} target="_blank" rel="noopener noreferrer">Ver más</a> </button>
+                {/* Social sharing options */}
+                <div className='Card-social'>
+                  <h5>Comparte:</h5>
+                  <div className="Card-social-btn">
+                    {/* Share on Facebook */}
+                    <button
+                      onClick={() => {
+                        const url = `https://www.facebook.com/sharer/sharer.php?u=${currentURL}`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-facebook-square'></i></button>
+                    {/* Share on Twitter */}
+                    <button
+                      onClick={() => {
+                        // Truncate title for Twitter sharing
+                        const truncatedText = news.Title.slice(0, 50);
+                        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(truncatedText)}&url=${currentURL}`;
+                        window.open(url, '_blank');
+                      }}
+                    ><i className='bx bxl-twitter' ></i></button>
+                  </div>
                 </div>
               </div>
+            ))}
+
+            {/* Controles de paginación */}
+            <div className="pagination-controls">
+              <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
+                Anterior
+              </button>
+              <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
+              <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= update.length}>
+                Siguiente
+              </button>
             </div>
-          ))}
-
-          {/* Controles de paginación */}
-          <div className="pagination-controls">
-            <button onClick={() => cambiarPagina(paginaActual - 1)} disabled={paginaActual === 1}>
-              Anterior
-            </button>
-            <h2 className='current-page'>{`Página ${paginaActual}`}</h2>
-            <button onClick={() => cambiarPagina(paginaActual + 1)} disabled={indiceFinal >= update.length}>
-              Siguiente
-            </button>
           </div>
-        </div>
-      ) : (
-        <Loading />
-      )}
-    </article>
-  );
-};
+        ) : (
+          <Loading />
+        )}
+      </article>
+    );
+  };
 
-export default Engineering;
+  export default Engineering;
