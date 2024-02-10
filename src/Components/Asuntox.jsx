8cb9c053dc068ef0d171e22aacbb90c4e4892c87
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-10 18:55:36 +0100

    editing description in asuntos

--- src/Components/Asuntox.jsx
+++ src/Components/Asuntox.jsx
@@ -6,7 +6,7 @@ const Asuntox = () => {
     const currentURL = 'https://unibarranquilla-newspaper.netlify.app/#/ASUNTOS';
 
     const dataTitle = 'MI UNIVERISIDAD, MI CIUDAD'
-    const dataDescription = '!Aquí te sumergimos en la vida estudiantil y el dinamismo urbano que define nuestra comunidad. Desde eventos académicos hasta iniciativas comunitarias, descubre cómo nuestra universidad y la ciudad se entrelazan para enriquecer nuestra experiencia y dejar una marca positiva en nuestra comunidad.'
+    const dataDescription = 'Aquí te sumergimos en la vida estudiantil y el dinamismo urbano que define nuestra comunidad. Desde eventos académicos hasta iniciativas comunitarias, descubre cómo nuestra universidad y la ciudad se entrelazan para enriquecer nuestra experiencia y dejar una marca positiva en nuestra comunidad.'
 
     return (
         <article className="engineering_section">
