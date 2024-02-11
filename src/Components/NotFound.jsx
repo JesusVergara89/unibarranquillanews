df8b340a95c33e4b631ce6f40a3a84cb1975183d
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-11 11:55:45 +0100

    addint home btn to 404 route to come back easy to home page

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../Styles/NotFound.css'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+    const navigateHome = useNavigate()
     return (
         <div className="error-container">
             <h1 className="error-heading">Error 404</h1>
             <p className="error-message">La página que estás buscando no se encuentra.</p>
+            <button onClick={() => navigateHome('/')} className="navigate-home">HOME</button>
         </div>
     )
 }
