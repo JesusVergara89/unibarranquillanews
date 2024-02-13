879967994431461633dbb4c007753a0f99e42704
Brian Escorcia <naziegonzalez5@gmail.com>
2024-02-13 14:07:04 -0500

    New headers

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,53 +2,6 @@ import React, { useState } from 'react'
 import '../Styles/header.css'
 import { Link } from 'react-router-dom'
 const Header = () => {
-<<<<<<< HEAD
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Desplazamiento suave
-    });
-  };
-  return (
-    <header className='Header'>
-      {/* Logo section */}
-      <div className="name_background">
-        <span className='L1'><h1>U</h1></span>
-        <span className='L2'><h1>N</h1></span>
-        <span className='L3'><h1>I</h1></span>
-        <span className='L4'><h1>B</h1></span>
-        <span className='L5'><h1>A</h1></span>
-        <span className='L6'><h1>R</h1></span>
-        <span className='L7'><h1>R</h1></span>
-        <span className='L8'><h1>A</h1></span>
-        <span className='L9'><h1>N</h1></span>
-        <span className='L10'><h1>Q</h1></span>
-        <span className='L11'><h1>U</h1></span>
-        <span className='L12'><h1>I</h1></span>
-        <span className='L13'><h1>L</h1></span>
-        <span className='L14'><h1>L</h1></span>
-        <span className='L15'><h1>A</h1></span>
-      </div>
-      {/* Menu section */}
-      <div className="menu">
-        {/* Navigation buttons */}
-        <button onClick={scrollToTop} ><Link to='/'>HOME</Link></button>
-        <button><Link to='/ACTUALIDAD'>ACTUALIDAD</Link></button>
-        <button><Link to='/CULTURA'>CULTURA</Link></button>
-        <button><Link to='/INVESTIGACION'>INVESTIGACION</Link></button>
-        <button><Link to='/DEPORTES'>DEPORTES</Link></button>
-        <button><Link to='/ASUNTOS'>ASUNTOS</Link></button>
-        <button><Link to='/VIDAU'>VIDA</Link></button>
-        <button><Link to='/EVENTOS'>EVENTO</Link></button>
-        <button><Link to='/ENTREVISTA'>ENTREVISTA</Link></button>
-        <button><Link to='/OPENPOSSITIONS'>WORKS</Link></button>
-        <button><Link to='/TECNOLOGIA'>TECNOLOGIA</Link></button> 
-        <button><Link to='/LOGIN'>LOGIN</Link></button>
-        <button><Link to='/COLLABORATORS'>COLLABORATORS</Link></button>        
-      </div> 
-    </header>
-  );
-=======
     const [Menu, setMenu] = useState(false)
     const menuLoad = () => { setMenu(!Menu) }
     return (
@@ -77,7 +30,6 @@ const Header = () => {
             <div onClick={menuLoad} className={Menu ? 'Close on' : 'Close off'} />
         </>
     )
->>>>>>> DeveloperCarr
 }
 
 export default Header
