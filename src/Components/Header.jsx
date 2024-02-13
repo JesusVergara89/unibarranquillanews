2e8681ea3007efac42e749ed035043f32951061b
Brian Escorcia <naziegonzalez5@gmail.com>
2024-02-13 11:32:46 -0500

    Remove icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import '../Styles/header.css'
 import { Link } from 'react-router-dom'
-import icons from '../Images/periodico.png'
 const Header = () => {
     const [Menu, setMenu] = useState(false)
     const menuLoad = () => { setMenu(!Menu) }
@@ -9,28 +8,26 @@ const Header = () => {
         <>
             <header className='Main_header'>
                 <Link className='Logo' to='/'>
-                    <img src={icons} alt="" />
                     <h1>UNIBARRANQUILLA</h1>
                 </Link>
                 <button onClick={menuLoad} className={Menu ? 'Menu_active on' : 'Menu_active'}>
-                    {
-                        Menu ? <i className='bx bx-x'></i> :
-                            <i className='bx bx-menu'></i>
-                    }
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </button>
             </header>
             <div className={Menu ? 'option on' : 'option off'}>
-                <Link className='enlace' to='/ACTUALIDAD'><i className='bx bx-alarm-add' />Actualidad </Link>
-                <Link className='enlace' to='/OPENPOSSITIONS'><i className='bx bx-conversation' /> Mi vida universitaria, mi ciudad </Link>
-                <Link className='enlace' to='/CULTURA'><i className='bx bx-palette' /> Cultura y Arte</Link>
-                <Link className='enlace' to='/VIDAU'><i className='bx bx-user' /> Vida Estudiantil</Link>
-                <Link className='enlace' to='/DEPORTES'><i className='bx bx-football' /> Deportes</Link>
-                <Link className='enlace' to='/EVENTOS'><i className='bx bx-calendar-event' /> Evento y Actividades</Link>
-                <Link className='enlace' to='/INVESTIGACION'><i className='bx bx-test-tube' /> Investigación y Desarrollo</Link>
-                <Link className='enlace' to='/ENTREVISTA'><i className='bx bx-briefcase-alt-2' /> Entrevista y perfiles </Link>
-                <Link className='enlace' to='/TECNOLOGIA'><i className='bx bx-rocket' /> Tecnología</Link>
+                <Link className='enlace' to='/ACTUALIDAD'>Actualidad </Link>
+                <Link className='enlace' to='/OPENPOSSITIONS'> Mi vida universitaria, mi ciudad </Link>
+                <Link className='enlace' to='/CULTURA'> Cultura y Arte</Link>
+                <Link className='enlace' to='/VIDA'> Vida Estudiantil</Link>
+                <Link className='enlace' to='/DEPORTES'> Deportes</Link>
+                <Link className='enlace' to='/EVENTOS'> Evento y Actividades</Link>
+                <Link className='enlace' to='/INVESTIGACION'> Investigación y Desarrollo</Link>
+                <Link className='enlace' to='/ENTREVISTA'> Entrevista y perfiles </Link>
+                <Link className='enlace' to='/TECNOLOGIA'> Tecnología</Link>
             </div>
-            <div onClick={menuLoad} className={Menu ?'Close on':'Close off'} />
+            <div onClick={menuLoad} className={Menu ? 'Close on' : 'Close off'} />
         </>
     )
 }
