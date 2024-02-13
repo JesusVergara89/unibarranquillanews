274e4d373a59dae87313ecbcb8a5001d1c6049f7
Brian Escorcia <naziegonzalez5@gmail.com>
2024-02-12 23:05:39 -0500

    New Social media

--- src/Components/Socialmedia.jsx
+++ src/Components/Socialmedia.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import '../Styles/Socialmedia.css'
+import twiter from '../Images/twitterx.svg'
 
 const Socialmedia = () => {
   return (
-<footer>
-  <div className="footer-inter">
-    <a href="https://www.instagram.com/unibarranquillanewspaper/" target="_blank">
-      <i className='bx bxl-instagram-alt'></i>
-    </a>
-    <a href="https://www.facebook.com/profile.php?id=61555932903808" target="_blank">
-      <i className='bx bxl-facebook'></i>
-    </a>
-    <a href="https://twitter.com/unibanewspaper" target="_blank">
-      <i className='bx bxl-twitter'></i>
-    </a>
-    <a href="mailto:unibarranquillanewspaper@gmail.com" target="_blank">
-      <i className='bx bxl-gmail'></i>
-    </a>
-  </div>
-</footer>
+    <footer>
+      <div className="footer-inter">
+        <a href="https://www.instagram.com/unibarranquillanewspaper/" target="_blank">
+          <i className='bx bxl-instagram'></i>
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=61555932903808" target="_blank">
+          <i className='bx bxl-facebook'></i>
+        </a>
+        <a href="https://twitter.com/unibanewspaper" target="_blank">
+          <img src={twiter} alt="" />
+        </a>
+        <a href="mailto:unibarranquillanewspaper@gmail.com" target="_blank">
+          <i className='bx bxl-gmail'></i>
+        </a>
+      </div>
+    </footer>
 
   )
 }
