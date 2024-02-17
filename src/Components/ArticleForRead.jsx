b2af54cd9bf97ed28c9baa1cf95415284b16cea2
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-17 15:11:24 +0100

    adding social media share btns

--- src/Components/ArticleForRead.jsx
+++ src/Components/ArticleForRead.jsx
@@ -6,6 +6,9 @@ import Loading from './Loading'
 import { db } from '../firebaseconfig'
 
 const ArticleForRead = () => {
+
+    const currentURL = window.location.href;
+
     const [articles, setArticles] = useState([{}])
     const [article, setArticle] = useState(null)
     const { id } = useParams()
@@ -43,6 +46,18 @@ const ArticleForRead = () => {
         })
     };
 
+    const compartirEnFacebook = () => {
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentURL)}`, '_blank');
+    };
+
+    const compartirEnTwitter = () => {
+        window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(currentURL)}`, '_blank');
+    };
+
+    const compartirEnLinkedIn = () => {
+        window.open(`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(currentURL)}`, '_blank');
+    };
+
     return (
         <div>
             {article ? (
@@ -66,6 +81,17 @@ const ArticleForRead = () => {
                         </div>
                         <div className="Complete-news-link">
                             <button className="Complet-news-link-btn"><a href={article.link} target="_blank" rel="noopener noreferrer">Link</a></button>
+                        </div>
+                        <div className="Complete-news-share-btns">
+                            <button onClick={compartirEnFacebook}><i className='bx bxl-facebook'></i>
+                            </button>
+                            <button onClick={compartirEnTwitter}>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" stroke="black" viewBox="0 0 50 50">
+                                    <path d="M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z"></path>
+                                </svg>
+                            </button>
+                            <button onClick={compartirEnLinkedIn}><i className='bx bxl-linkedin'></i>
+                            </button>
                         </div>
                     </div>
                 </article>
