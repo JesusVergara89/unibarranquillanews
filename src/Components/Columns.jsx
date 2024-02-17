2ce50f904801029d8857ff2832fdcbfd5532f0d3
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-17 14:32:21 +0100

    changing the sections photos to local repository

--- src/Components/Columns.jsx
+++ src/Components/Columns.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import '../Styles/Columns.css'
 import { useNavigate } from 'react-router-dom'
+import actualidad from '../Images/people.jpg'
+import cultura from '../Images/arte.jpg'
+import deportes from '../Images/deporte.jpg'
+import investigación from '../Images/microscope.jpg'
+import asuntos from '../Images/city.jpg'
+import vidau from '../Images/estudiantes.jpg'
+import evento from '../Images/evento.jpg'
+import entrevista from '../Images/entrevista.jpg'
+import tecnologia from '../Images/tech.jpg'
+
 
 const Columns = () => {
 
@@ -26,55 +36,55 @@ const Columns = () => {
     return (
         <div className="container">
             <button className='outside-btn' onClick={() => navigateEngi('/ACTUALIDAD')}  >
-                <img onClick={() => navigateEngi('/ACTUALIDAD')} src="https://photo-data-base.netlify.app/assets/people.e4fef31b.jpg" />
+                <img onClick={() => navigateEngi('/ACTUALIDAD')} src={actualidad} />
                 <h2 >Ver más</h2>
                 <h3>Actualidad</h3>
             </button>
 
             <button id='outside-btn' onClick={() => navigateTravel('/CULTURA')}  >
-                <img onClick={() => navigateTravel('/CULTURA')} src="https://photo-data-base.netlify.app/assets/arte.a47a54ae.jpg" />
+                <img onClick={() => navigateTravel('/CULTURA')} src={cultura} />
                 <h2 >Ver más</h2>
                 <h3>Cultura y Arte</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateLiterature('/DEPORTES')} >
-                <img onClick={() => navigateLiterature('/DEPORTES')} src="https://photo-data-base.netlify.app/assets/deporte.01eed798.jpg" />
+                <img onClick={() => navigateLiterature('/DEPORTES')} src={deportes} />
                 <h2 >Ver más</h2>
                 <h3>Deportes</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateExperience('/INVESTIGACION')} >
-                <img onClick={() => navigateExperience('/INVESTIGACION')} src="https://photo-data-base.netlify.app/assets/microscope.39879e23.jpg" />
+                <img onClick={() => navigateExperience('/INVESTIGACION')} src={investigación} />
                 <h2 >Ver más</h2>
                 <h3>Investigación y Desarrollo</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateAsuntos('/ASUNTOS')} >
-                <img onClick={() => navigateAsuntos('/ASUNTOS')} src="https://photo-data-base.netlify.app/assets/city.7f4b3f45.jpg" />
+                <img onClick={() => navigateAsuntos('/ASUNTOS')} src={asuntos} />
                 <h2 >Ver más</h2>
                 <h3>Mi universidad, mi ciudad</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateVida('/VIDAU')} >
-                <img onClick={() => navigateVida('/VIDAU')} src="https://photo-data-base.netlify.app/assets/estudiantes.1638c5fc.jpg" />
+                <img onClick={() => navigateVida('/VIDAU')} src={vidau} />
                 <h2 >Ver más</h2>
                 <h3>Vida Estudiantil</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateEvento('/EVENTOS')} >
-                <img onClick={() => navigateEvento('/EVENTOS')} src="https://photo-data-base.netlify.app/assets/evento.99182bf6.jpg" />
+                <img onClick={() => navigateEvento('/EVENTOS')} src={evento} />
                 <h2 >Ver más</h2>
                 <h3>Eventos y Actividades</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateEntrevista('/ENTREVISTA')} >
-                <img  onClick={() => navigateEntrevista('/ENTREVISTA')} src="https://photo-data-base.netlify.app/assets/entrevista.edf8a3c4.jpg" />
+                <img  onClick={() => navigateEntrevista('/ENTREVISTA')} src={entrevista} />
                 <h2 >Ver más</h2>
                 <h3>Entrevistas y Perfiles</h3>
             </button>
 
             <button className='outside-btn' onClick={() => navigateTecnologia('/TECNOLOGIA')} >
-                <img  onClick={() => navigateEntrevista('/TECNOLOGIA')} src="https://photo-data-base.netlify.app/assets/tech.cd4fc329.jpg" />
+                <img  onClick={() => navigateEntrevista('/TECNOLOGIA')} src={tecnologia} />
                 <h2 >Ver más</h2>
                 <h3>Tecnología</h3>
             </button>
