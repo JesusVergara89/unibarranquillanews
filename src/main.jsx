7f6287cd1a8a95e1123308382b5a97d6307ded2f
Brian Escorcia <naziegonzalez5@gmail.com>
2024-02-23 18:45:32 -0500

    remove browserrouter for hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import Blog from './Blog.jsx'
 import './index.css'
 import store from './store';
 import { Provider } from 'react-redux'
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <Blog />
       <ToastContainer />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 
 )
