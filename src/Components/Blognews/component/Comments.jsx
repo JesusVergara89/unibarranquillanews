9d8cd05350a42400dde1ebf0899add7ad3647294
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-02-24 20:23:53 +0100

    comment section and styles test

--- src/Components/Blognews/component/Comments.jsx
+++ src/Components/Blognews/component/Comments.jsx
@@ -1,18 +1,94 @@
-import { doc, onSnapshot } from 'firebase/firestore'
+import { arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { db1 } from '../../../firebaseconfig'
+import { auth, db1 } from '../../../firebaseconfig'
+import '../styles/Comments.css'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { v4 as uuidv4 } from 'uuid'
 
-const Comments = ({id}) => {
-    const [comments, setComments] = useState([])
-    useEffect(()=>{
-        const docRef = doc(db1, "Articles", id)
-        onSnapshot(docRef, (snapshot)=>{
-            setComments(snapshot.data().comments)
+const Comments = ({ id }) => {
+
+  const [comment, setComment] = useState('')
+  const [comments, setComments] = useState([])
+  const [currentlyLoggedinUser] = useAuthState(auth)
+  const commentRef = doc(db1, "Articles", id)
+
+  useEffect(() => {
+    const docRef = doc(db1, "Articles", id)
+    onSnapshot(docRef, (snapshot) => {
+      setComments(snapshot.data().comments)
+    })
+  }, [])
+
+  const handleChangeComment = (e) => {
+    if (e.key === "Enter") {
+      updateDoc(commentRef, {
+        comments: arrayUnion({
+          user: currentlyLoggedinUser.uid,
+          userName: currentlyLoggedinUser.displayName,
+          comment: comment,
+          createdAt: new Date(),
+          commentId: uuidv4()
         })
-    },[])
+      })
+        .then(
+          () => {
+            setComment('')
+          })
+    }
+  }
+
+  const handledeletecoment = (comment) => {
+    console.log(comment)
+  }
+
   return (
     <article className="comment-section">
-        comments 
+      <div className="Comments-div">
+        {comments !== null &&
+          comments.map(
+            ({ commentId, user, comment, userName, createdAt }) => (
+              <div key={commentId} className="comment-card">
+                <div className="display-comment">
+                  {comment}
+                </div>
+                <div className="comment-user-information">
+                  <div className={`comment-user ${user === currentlyLoggedinUser.uid ? "green" : "red"}`}>{userName}</div>
+                  <div className="comment-date">{createdAt.toDate().toDateString()}</div>
+                </div>
+                <div className="delete-btn">
+                  {
+                    user === currentlyLoggedinUser.uid &&
+                    (
+                      <i className="fa fa-times"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handledeletecoment(comment)}
+                      ></i>
+                    )
+                  }
+                </div>
+              </div>
+            )
+          )
+
+        }
+      </div>
+
+      <div className="Comments-make">
+        {
+          currentlyLoggedinUser &&
+          (
+            <textarea
+             placeholder='Presiona enter para comentar'
+              type='text'
+              className='create-comment'
+              value={comment}
+              onChange={(e) => { setComment(e.target.value) }}
+              onKeyUp={(e) => { handleChangeComment(e) }}
+            />
+          )
+        }
+      </div>
+
     </article>
   )
 }
