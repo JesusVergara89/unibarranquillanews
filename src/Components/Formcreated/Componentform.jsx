34e9b1c4421b45022944194d0483b86372fa2648
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-03-13 13:36:43 +0100

    adding enterregister || off ? off to the conditions of componentform

--- src/Components/Formcreated/Componentform.jsx
+++ src/Components/Formcreated/Componentform.jsx
@@ -37,7 +37,7 @@ const Componentform = (
     return (
         <article className="component-form">
 
-            {enterregister ?
+            {enterregister || off ?
                 ''
                 :
                 <div className='Container-post-form'>
