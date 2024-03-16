4f7a8d1eceeb3e5e066cf8208f1d3b4d0d505a75
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-03-16 20:54:08 +0100

    adding new images but svgs for the sections in science

--- src/Components/Ciencias.jsx
+++ src/Components/Ciencias.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import '../Styles/Ciencias.css'
 import SectionPage from './ScienceComponents/SectionPage'
 import Cardscience from './ScienceComponents/Cardscience'
+import fisica from '../Images/fisica.svg'
+import math from '../Images/mathimg.svg'
 
 const Ciencias = () => {
   const [subjects, setSubjects] = useState([{ name: "Math", active: true }, { name: "Physics", active: true }]);
@@ -21,13 +23,15 @@ const Ciencias = () => {
     });
   }
 
+  const imgForCardScience = [math,fisica ]
+
   return (
     <article className="ciencia">
       {selectedSubject ? '' :
         <div className="ciencia-cards">
           {subjects.map((subject, index) => (
             subject.active &&
-            <Cardscience key={index} index={index} actualizarEstado={actualizarEstado} subject={subject} />
+            <Cardscience key={index} imgForCardScience={imgForCardScience[index]} index={index} actualizarEstado={actualizarEstado} subject={subject} />
           ))}
         </div>
       }
--- src/Components/ScienceComponents/Cardscience.jsx
+++ src/Components/ScienceComponents/Cardscience.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Cardscience.css'
 
-const Cardscience = ({actualizarEstado, subject, index}) => {
+const Cardscience = ({actualizarEstado, subject, index, imgForCardScience}) => {
     return (
         <div className="Card-science"
             onClick={() => actualizarEstado(index)}
         >
+            <img src={imgForCardScience} alt="" />
             <h2 className='Card-science-h2'
                 onClick={() => actualizarEstado(index)}
             >{subject.name}</h2>
